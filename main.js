06c8ed26133a0d112d854694c49baf9ec8298b78
delmsnoob <delmsnoob@gmail.com>
2021-12-06 01:25:49 +0800

    event listener

---
+++ main.js
@@ -0,0 +1,25 @@
+var app = new Vue ({
+    el: '#app',
+    data: {
+        product: "Socks",
+        image: "./assets/images/socks_green.jpg",
+        inStock: true,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green",
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue",
+            }
+        ],
+        cart: 0,
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+    }
+})
